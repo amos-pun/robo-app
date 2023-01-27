4782c2b77bf2c77abb6ca151987d1a98d1b44ac0
Your Name <amos.pun1994@gmail.com>
2023-01-27 19:35:47 +0545

    PORT = 3009, card has been made

---
+++ src/Cards.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/1" height={"200px"} alt="img.."/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
